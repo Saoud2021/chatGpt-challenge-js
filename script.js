4153279f0cc8d018562b3a66fb936e18784e15b7
Saoud2021 <abdallahnasser2015@gmail.com>
2024-11-18 19:45:51 +0200

    commit

--- script.js
+++ script.js
@@ -6,27 +6,36 @@ const restartBtn = document.querySelector(".restart");
 
 let computerGuess = Math.round(Math.random() * 100);
 let attempts = 0;
+let savedAttempts = [];
 
 function guess() {
-  let UserGuess = Number(inputEl.value);
+  let userGuess = Number(inputEl.value);
+  if (userGuess < 1 || userGuess > 100) {
+    resultEl.innerHTML = "Please enter a number between 1 and 100.";
+    return;
+  }
   if (attempts === 10) {
     resultEl.innerHTML = "you are out of attempts try again !!";
     return;
   }
-  if (UserGuess < 1 || UserGuess > 100) {
-    resultEl.innerHTML = "Please enter a number between 1 and 100.";
-    return;
-  }
-  if (computerGuess > UserGuess) {
+
+  if (computerGuess > userGuess) {
     resultEl.innerHTML = "Your guess is low";
-  } else if (computerGuess === UserGuess) {
+  } else if (computerGuess === userGuess) {
     resultEl.innerHTML = " CONGRATS !! YOUR GUESS IS RIGHT ";
     return;
-  } else if (computerGuess < UserGuess) {
+  } else if (computerGuess < userGuess) {
     resultEl.innerHTML = "your guess is high";
   } else {
     resultEl.innerHTML = "Not a valid input";
   }
+  if (savedAttempts.includes(userGuess)) {
+    resultEl.innerHTML = "Try a different guess";
+    return;
+  }
+
+  savedAttempts.push(userGuess);
+
   attempts++;
   attemptsEl.innerHTML = 10 - attempts;
 }
@@ -34,8 +43,8 @@ function guess() {
 function resetGame() {
   attempts = 0;
   attemptsEl.innerHTML = 10;
-  inputEl.value = " ";
-  resultEl.innerHTML = " ";
+  inputEl.value = "";
+  resultEl.innerHTML = "";
   computerGuess = Math.round(Math.random() * 100);
 }
 restartBtn.addEventListener("click", resetGame);
